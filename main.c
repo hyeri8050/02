cb43307894e397fa6694654b9902fbae9ae442bb
Hyeri Park <phr66@naver.com>
2023-09-14 16:39:48 +0900

    week2

--- main.c
+++ main.c
@@ -4,7 +4,12 @@
 int main(int argc, char *argv[])
 {
   printf("Hello, Sookmyung Electronics! \n");
-  printf("3+4 = %i\n", 3+4); 
+   
+  int a, b; //정수 변수 선언
+  a = 10;
+  b = 20;
+  printf("%d + %d = %d\n", a, b, a+b); 
+  
   getchar(); //Prevents execution from ending quickly
   return 0;
 }
